b3feaa49a171a01c41667b62a7e4b6d55f940fcd
LuanSantosSilva <luan48924@gmail.com>
2022-10-19 11:56:24 -0300

    modificacoes

--- cooker/views.py
+++ cooker/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from utils.cooker.factory import make_recipe
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'cooker/pages/home.html', context={'name':'Luan'})
+    return render(request, 'cooker/pages/home.html', context={'recipes':[make_recipe() for _ in range(10)]})
+
+def recipe(request, id):
+    return render(request, 'cooker/pages/recipe-view.html', context={
+        'recipe':make_recipe(),
+        'is_detail_page': True,
+    })
